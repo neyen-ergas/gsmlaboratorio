6316b25d65746df580785f5f1ac6c633efbb09d1
Neyen Ergas <neyenergas@gmail.com>
2023-08-28 12:01:47 -0300

    subida codigo

---
+++ src/test/java/farmacia/gsm/laboratorio/LaboratorioApplicationTests.java
@@ -0,0 +1,13 @@
+package farmacia.gsm.laboratorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
