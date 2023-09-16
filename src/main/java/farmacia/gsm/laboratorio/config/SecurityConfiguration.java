51498850c35d4f8ff17d08b1eceaa1d7d0574818
robyspr <robysprenger@gmail.com>
2023-09-16 16:17:36 -0300

    Add authentication manager and provider

--- src/main/java/farmacia/gsm/laboratorio/config/SecurityConfiguration.java
+++ src/main/java/farmacia/gsm/laboratorio/config/SecurityConfiguration.java
@@ -1,16 +1,35 @@
 package farmacia.gsm.laboratorio.config;
 
+import farmacia.gsm.laboratorio.services.AuthorizationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final AuthorizationService authorizationService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(authorizationService.userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- src/main/java/farmacia/gsm/laboratorio/exceptions/ErrorCode.java
+++ src/main/java/farmacia/gsm/laboratorio/exceptions/ErrorCode.java
@@ -2,5 +2,6 @@ package farmacia.gsm.laboratorio.exceptions;
 
 public enum ErrorCode {
     WRONG_METHOD, DUPLICATED_VALUE, INVALID_VALUE,
+    BAD_CREDENTIALS,
     USER_DOESNT_EXIST
 }
